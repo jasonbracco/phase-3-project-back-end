03ec33009fcc1f31c3f26e803205326a170fe42c
Jason Bracco <bracco.jason@gmail.com>
2022-10-05 18:33:33 -0400

    forgot to add in id for delete request

--- Project-Server/app/controllers/application_controller.rb
+++ Project-Server/app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < Sinatra::Base
     shoe.to_json
   end
 
-  delete '/shoes' do 
+  delete '/shoes:id' do 
     shoe=Shoe.find(params[:id])
     shoe.destroy
     shoe.to_json
